1946f306f71d5c787e26fb07d8aa48c325928088
limichange <limichange@hotmail.com>
2022-02-21 10:47:27 +0800

    fix: clear test code and import libs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
-const player = document.querySelector('lottie-player')
-
-player.setSubframe(false)
+var animation = lottie.loadAnimation({
+  container: document.getElementById('lottie'), // Required
+  path: './lottiefiles/lf20_lrx1ahrj.json', // Required
+  renderer: 'html', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+})
