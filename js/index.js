7a0215d85cb95568a8da03c429e4d41f340ecddf
limichange <limichange@hotmail.com>
2022-02-21 10:48:58 +0800

    chore: test canvas

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var animation = lottie.loadAnimation({
   container: document.getElementById('lottie'), // Required
   path: './lottiefiles/lf20_lrx1ahrj.json', // Required
-  renderer: 'html', // Required
+  renderer: 'canvas', // Required
   loop: true, // Optional
   autoplay: true, // Optional
 })
