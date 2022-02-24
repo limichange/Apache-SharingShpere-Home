d5c646d303d0d30b9151b227bf95d4ec657d889f
limichange <limichange@hotmail.com>
2022-02-24 14:38:09 +0800

    feat: Roadmap content

--- js/index.js
+++ js/index.js
@@ -10,3 +10,39 @@ let animation = lottie.loadAnimation({
 })
 
 animation.setSubframe(false)
+
+$(document).ready(function () {
+  $('.roadmapSlick').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  })
+})
