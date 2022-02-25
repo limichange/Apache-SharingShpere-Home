3de4d113e3ddc8447e1e25651b6c0d54d769cf46
Xiao Du <duxiao@dinehq.com>
2022-02-25 12:20:32 +0800

    fix: rename lottie json

--- js/index.js
+++ js/index.js
@@ -2,17 +2,17 @@
 // First Section
 //
 let animation = lottie.loadAnimation({
-  container: document.getElementById('lottie'),
-  path: './lottiefiles/lf20_lrx1ahrj.json',
-  renderer: 'svg',
+  container: document.getElementById("lottie"),
+  path: "./lottiefiles/hero.json",
+  renderer: "svg",
   loop: true,
   autoplay: true,
-})
+});
 
-animation.setSubframe(false)
+animation.setSubframe(false);
 
 $(document).ready(function () {
-  $('.roadmapSlick').slick({
+  $(".roadmapSlick").slick({
     infinite: false,
     speed: 300,
     variableWidth: true,
@@ -28,50 +28,50 @@ $(document).ready(function () {
         },
       },
     ],
-  })
+  });
 
-  $('.roadmapSlick').on(
-    'beforeChange',
+  $(".roadmapSlick").on(
+    "beforeChange",
     function (event, slick, currentSlide, nextSlide) {
       if (nextSlide > 0) {
-        $('.shadow.left').show()
+        $(".shadow.left").show();
       } else {
-        $('.shadow.left').hide()
+        $(".shadow.left").hide();
       }
 
-      var rightEndIndex = 4
+      var rightEndIndex = 4;
 
       // get window width
-      var windowWidth = $(window).width()
+      var windowWidth = $(window).width();
 
       if (windowWidth > 1368) {
-        rightEndIndex = 2
+        rightEndIndex = 2;
       } else if (windowWidth > 1068) {
-        rightEndIndex = 3
+        rightEndIndex = 3;
       }
 
       if (nextSlide > rightEndIndex) {
-        $('.slick-next.slick-arrow').hide()
+        $(".slick-next.slick-arrow").hide();
       } else {
-        $('.slick-next.slick-arrow').show()
+        $(".slick-next.slick-arrow").show();
       }
     }
-  )
+  );
 
-  $('.shadow.left').hide()
+  $(".shadow.left").hide();
 
-  $('.navigation .iconMenu').click(function () {
-    $('.mobileMenu').show()
-  })
+  $(".navigation .iconMenu").click(function () {
+    $(".mobileMenu").show();
+  });
 
-  $('.languageSelect + .langs').hide()
+  $(".languageSelect + .langs").hide();
 
-  $('.mobileMenu .x').click(function () {
-    $('.mobileMenu').hide()
-  })
+  $(".mobileMenu .x").click(function () {
+    $(".mobileMenu").hide();
+  });
 
-  $('.languageSelect').click(function () {
-    $('.languageSelect + .langs').toggle()
-    $('.languageSelect .arrowIcon').toggleClass('active')
-  })
-})
+  $(".languageSelect").click(function () {
+    $(".languageSelect + .langs").toggle();
+    $(".languageSelect .arrowIcon").toggleClass("active");
+  });
+});
